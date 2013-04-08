2d6d909ee539a89b5be5f2f06e854b7e2e3dee53
Eric Mattison <ericmattison@Erics-MacBook-Pro.local>
2013-04-08 14:50:29 -0400

    Added prod settings

--- admin_apps/settings/prod.py
+++ admin_apps/settings/prod.py
@@ -4,6 +4,10 @@ from admin_apps.settings.base import *   # pylint: disable=W0614,W0401
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+PW_FILE = '/var/www/pw'
+f = open(PW_FILE)
+un, pw = f.read().strip().split(':')
+
 VAR_ROOT = '/var/www/admin_apps'
 MEDIA_ROOT = os.path.join(VAR_ROOT, 'uploads')
 STATIC_ROOT = os.path.join(VAR_ROOT, 'static')
@@ -12,8 +16,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'admin_apps',
-        'USER': 'dbuser',
-        'PASSWORD': 'dbpassword',
+        'USER': un,
+        'PASSWORD': pw,
     }
 }
 
